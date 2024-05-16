074149bdf28d5654f8dca29d5f40c72c8ef415c5
Tracie Smith <90426413+tsmit655@users.noreply.github.com>
2024-05-16 10:22:26 -0400

    checkpoint

--- script/menu.js
+++ script/menu.js
@@ -18,24 +18,50 @@ let menu = {
 
 
    // input 
-   const menuList =  document.getElementById("menuList");
+   const menuCategoryDropdown = document.getElementById("menuCategoryDropdown");
+   const menuList = document.getElementById("menuList");
+
+   
    
    //output
    const outputMenu = document.getElementById("outputMenu");
 
-   window.onload = function(){
-    populateMenuList()
-   };
-
-   function populateMenuList(){
-    console.log("populate")
-    
-
-   // Add default option
-   let defaultOption = document.createElement("option");
-   defaultOption.textContent = "Select an option";
-   defaultOption.value = '';
-   menuList.appendChild(defaultOption);
+   window.onload = () => {
+    menuCategoryDropdown.onchange = onMenuCategoryDropdownChange;
    }
+   function onMenuCategoryDropdownChange(){
+    
+    //figure out what value was selected (which category)
+    let selectedCategory = menuCategoryDropdown.value;
+    console.log(selectedCategory);
+
+    // clear out menuList from previous selection
+    menuList.options.length = 0;
+
+     // loop through the correct array an add values to the toyList one at a time
+     let itemsInCategory = getItemsForCategory(selectedCategory);
+    console.log(itemsInCategory);
+
+    itemsInCategory.forEach(addItemsToMenuList);
+
+    function addItemsToMenuList(menuItem) {
+        let newOption = document.createElement("option");
+        newOption.value = menuItem;
+        newOption.innerHTML = menuItem;
+        menuList.appendChild(newOption);
+
+   }
+
+   function getItemsForCategory(categoryCode) {
+    if (categoryCode === "drinks") {
+        return menu.drinks;
+    } else if (categoryCode === "entrees") {
+        return menu.entrees;
+    } else if (categoryCode === "desserts") {
+        return menu.desserts;
+    }
+    return [];
+}
+
    
-   
+}
